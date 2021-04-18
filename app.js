0c0d1df9bdd77d77baba6a5d160adf99e577347d
Benon <66958303+Machfn@users.noreply.github.com>
2021-04-18 05:02:32 -0600

    Uploaded the files

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            sike: "Benon",
+            pog: "PoggerHamster420",
+            imgguy: "Ciaran O'Brien",
+        }
+    }
+})
+
+app.mount('#app')
