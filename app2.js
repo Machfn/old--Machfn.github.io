d19c2df80f61f952b9f3ff0ac56d41514eb8040a
Benon <66958303+Machfn@users.noreply.github.com>
2021-04-20 13:49:49 -0600

    Implementing mobile version
    Poggers

---
+++ app2.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            sike: "Benon",
+            pog: "PoggerHamster420",
+            imgguy: "Ciaran O'Brien",
+            imgpeople: "Renan Kamikoga",
+        }
+    }
+})
+
+app.mount('#app')
---
+++ mobileindex.js
@@ -0,0 +1,25 @@
+function openeddrive() {
+    console.log("User opened their drive")
+}
+function openedclassroom() {
+    console.log("User opened Classroom")
+}
+function openedproton() {
+    console.log("User has opened proton mail")
+}
+function openedschoolzone() {
+    console.log("User has opened their Schoolzone")
+}
+function openedgmail() {
+    console.log("User has opened their inbox")
+}
+function openall() {
+    window.open("https://classroom.google.com");
+    window.open("https://drive.google.com");
+    window.open("https://mail.google.com");
+    window.open("https://protonmail.com/");
+    window.open("https://schoolzone.epsb.ca/cf/index.cfm");
+}
+function openedall() {
+    console.log("User opened all")
+}
